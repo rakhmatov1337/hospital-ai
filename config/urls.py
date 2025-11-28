493449feae295373ff6ac10a9632252f696a3fb8
Nazrulloh <rakhmatovnazrulloh@gmail.com>
2025-11-28 16:59:33 +0500

    static added

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
@@ -36,3 +38,7 @@ urlpatterns = [
     path('api/auth/', include('accounts.urls')),
     path('api/hospitals/', include('hospitals.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
